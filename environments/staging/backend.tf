05256f7125b3292ecd86215d90a17a2209b492d8
taj <69720183+Taj3755-de@users.noreply.github.com>
2025-10-29 18:45:53 +0530

    Update backend.tf

--- environments/staging/backend.tf
+++ environments/staging/backend.tf
@@ -1,7 +1,7 @@
 
 terraform {
   backend "s3" {
-    bucket        = "tajudeen-tf-state-us-east-1-CHANGE-ME"
+    bucket        = "tajudeen-tf-state-us-east-1"
     key           = "staging/terraform.tfstate"
     region        = "us-east-1"
     encrypt       = true
